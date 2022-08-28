807cede4579d8d7d540caecf34ca7262818f548e
yeyaolong <470080502@qq.com>
2022-08-28 20:58:54 +0800

    暂存

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 use wasm_bindgen::prelude::*; // JS与Rust交互的包
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
