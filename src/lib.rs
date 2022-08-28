794e6c2f9867982c0b8eff93f87b7e9ed5d0d06b
yeyaolong <470080502@qq.com>
2022-08-28 18:09:40 +0800

    新建项目

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use wasm_bindgen::prelude::*; // JS与Rust交互的包
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn hello(name: &str) {
+    alert(name);
+}
+
---
+++ www/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "../public"),
+        filename: "index.js",
+    },
+    mode: "development",
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "../index.html"),
+                    to: path.resolve(__dirname, "../public")
+                }
+            ]
+        })
+    ]
+}
