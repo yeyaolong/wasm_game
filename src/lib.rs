d0c30f4c7a5ee6d8f061db5764a3ed84c4f8beb2
yeyaolong <470080502@qq.com>
2022-08-29 06:46:48 +0800

    创建蛇头

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,53 @@ pub fn hello(name: &str) {
     alert(name);
 }
 
+
+#[wasm_bindgen]
+pub struct World {
+    // 定义一个结构体，这个结构体包含两个参数，width和size
+    // 两个参数的类型都是usize类型(无符号整型数据，位数大小由操作系统决定，比如我的电脑是64位的，那usize就占有64bit)
+    width: usize,
+    size: usize,
+    snake: Snake,
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+
+}
+
+impl Snake {
+    /**
+      * spawn_index 初始点位
+      */
+    fn new(spawn_index: usize) -> Self {
+        Self {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize) -> Self {
+        Self {
+            width,
+            size: width * width, // 创建一个长宽相等的正方形
+            snake: Snake::new(13), // 创建一条在13位置的蛇
+        }
+    }
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_index(&self) -> usize {
+        // 不要写分号，不然报错
+        self.snake.body[0].0
+    }
+    pub fn update(&mut self) {
+        let snake_head_index: usize = self.snake_head_index();
+        self.snake.body[0].0 = (snake_head_index+1) % self.size;
+    }
+}
--- www/index.js
+++ www/index.js
@@ -1,6 +1,76 @@
-import init, { hello } from "wasm_game";
+import init, { World } from "wasm_game";
 
 init().then(() => {
-    hello("KappaFish");
-    console.log("Ok");
+    const CELL_SIZE = 20; // 定义一个格子的大小
+    
+    const world = World.new(8); // 一个16 * 16 的世界
+    const worldWidth = world.width();
+    const fps = 5;
+    const canvas = document.getElementById("snake-world");
+
+    const context = canvas.getContext("2d");
+
+    canvas.width = worldWidth * CELL_SIZE;
+    canvas.height = worldWidth * CELL_SIZE;
+    // 画一个16 * 16的网格
+    function drawWorld() {
+        context.beginPath();
+         // 画横线
+        for (let x = 0; x <= worldWidth; x++) {
+            context.moveTo(CELL_SIZE * x, 0); 
+            context.lineTo(CELL_SIZE * x, CELL_SIZE * worldWidth);
+        }
+         // 画竖线
+        for (let y = 0; y <= worldWidth; y++) {
+            context.moveTo(0, CELL_SIZE * y);
+            context.lineTo(CELL_SIZE * worldWidth, CELL_SIZE * y);
+
+        }
+
+        
+        context.stroke();
+        
+    }
+
+    // 画蛇头
+    function drawSnake() {
+        const snake_index = world.snake_head_index();
+        const row = Math.floor(snake_index / worldWidth);
+        const col = snake_index % worldWidth;
+
+        context.beginPath();
+
+        context.fillRect(
+            col * CELL_SIZE, // x
+            row * CELL_SIZE, // y
+            CELL_SIZE,
+            CELL_SIZE
+        )
+
+
+        context.stroke();
+    }
+
+
+    function draw() {
+        drawWorld();
+        drawSnake();
+    }
+
+    function run() {
+        // 视频里用的setTimeout里套requestAnimationFrame,
+        // 虽然我觉得直接window.requestAnimationFrame会更好一点
+        // 但实际上，requestAnimationFrame似乎有点太快了
+        // 所以这里只好外层套一个setTimeout用来减速
+        setTimeout(() => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            world.update();
+            draw();
+            requestAnimationFrame(run)
+        }, 1000 / fps);
+    }
+
+    draw();
+    run();
+
 })
